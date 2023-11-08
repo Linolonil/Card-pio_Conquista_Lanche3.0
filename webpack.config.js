aa43aea6d5df6fd65881e2eace8bf765f82c1895
Linolonil <linox999@gmail.com>
2023-11-08 10:05:39 -0400

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+// webpack.config.js
+
 const path = require("path");
 const WorkboxWebpackPlugin = require("workbox-webpack-plugin");
 
@@ -17,25 +19,6 @@ const config = {
       host: "localhost",
     },
     plugins: [new WorkboxWebpackPlugin.GenerateSW()],
-    module: {
-      rules: [
-        {
-          test: /\.css$/i,
-          use: [stylesHandler, "css-loader"],
-        },
-        {
-          test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
-          type: "asset",
-        },
-        {
-          test: /\.(js|jsx)$/,
-          exclude: /node_modules/,
-          use: {
-            loader: "babel-loader",
-          },
-        },
-      ],
-    },
   },
 };
 
